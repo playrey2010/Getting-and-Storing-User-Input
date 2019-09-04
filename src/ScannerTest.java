d877a869db0e3cd448043202f7b6614b2d9eb944
playrey2010 <playrey2010@gmail.com>
2019-09-04 12:01:16 -0400

    getting user input

---
+++ src/ScannerTest.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ScannerTest {
+    public static void main(String[] args) {
+        String firstInitial;
+        String lastName;
+        Integer houseNumber;
+        String streetName;
+        String streetType;
+        String city;
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Input the String from the instructions: (P Sherman 42 Wallaby Way Sydney)");
+        firstInitial = sc.next();
+        lastName = sc.next();
+        houseNumber = sc.nextInt();
+        streetName = sc.next();
+        streetType = sc.next();
+        city = sc.next();
+
+        System.out.print(firstInitial + " " + lastName + " " + houseNumber + " ");
+        System.out.println(streetName + " " + streetType + " " + city);
+        System.out.println("firstInitial: " + firstInitial + "\nlastName: " + lastName
+                            + "\nhouseNumber: " + houseNumber);
+    }
+}
